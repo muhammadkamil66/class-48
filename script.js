4cc2972ad44a721b116eb03c3eecbec457edbbaa
Muhammad Kamil <38943329+muhammadkamil66@users.noreply.github.com>
2018-07-26 01:09:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+let jobTitle = "manager";
+let geoLocation = "karachi";
+let favCar = "parado";
+
+console.log("you will be a "+jobTitle+" in "+geoLocation+" and you will drive a "+favCar);
+*/
+
+
+function initial(){
+	let jobTitle = `Manager`;
+	let geoLocation = `Karachi`;
+	let favCar = "Parado";
+
+	console.log(`you will be a  ${jobTitle} in ${geoLocation} and you will drive a ${favCar}`);
+
+}
